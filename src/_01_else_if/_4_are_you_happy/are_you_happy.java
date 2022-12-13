84b9e7c1f4466cd0825cc5286f710fa8709d6319
ryansmith <ryansmith@ryans-air.lan1>
2022-12-12 17:57:53 -0800

    Are You Happy

--- src/_01_else_if/_4_are_you_happy/are_you_happy.java
+++ src/_01_else_if/_4_are_you_happy/are_you_happy.java
@@ -1,5 +1,7 @@
 package _01_else_if._4_are_you_happy;
 
+import javax.swing.JOptionPane;
+
 public class are_you_happy {
 
 	public static void main(String[] args) {
@@ -7,4 +9,53 @@ public class are_you_happy {
 
 	}
 
+	
+
+JOptionPane.showMessageDialog(null,"Please answer with only 'yes' or 'no.'");	
+
+String q1 = JOptionPane.showInputDialog(null, "Are you happy?");	
+
+if(q1.equals("yes")) {
+	JOptionPane.showMessageDialog(null, "Keep doing whatever you are doing.");
+	
+}
+
+if(q1.equals("no")) {
+	
+	String q2 = JOptionPane.showInputDialog(null, "Do you want to be happy?");
+	
+	 if(q2.equals("no")) {
+		JOptionPane.showMessageDialog(null, "Keep doing whatever you are doing.");
+	}
+		 if(q2.equals("yes")) {
+			JOptionPane.showMessageDialog(null, "Change something.");
+	
+		
+		
+		}
+		
+}
+			
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
